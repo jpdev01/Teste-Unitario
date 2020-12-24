31cce89bb4a5498d6b3695e1bf7ac8274b388ff4
João Pedro Truchinski Borba <jptruchinski@gmail.com>
2020-12-23 22:44:08 -0300

    Exemplo de soma no metodo main de calculator...ok

--- src/calculator/calculator.java
+++ src/calculator/calculator.java
@@ -3,6 +3,7 @@ package calculator;
 public class calculator {
 
     public static void main(String[] args){
-
+        System.out.println("-----CALCULADORA-------");
+        System.out.println(operations.somar(2, 4));
     }
 }
